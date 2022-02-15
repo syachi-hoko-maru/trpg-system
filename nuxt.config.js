3c2d8a8de550b9ceef901e402590cca4466b8c28
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2022-02-15 13:22:39 +0900

    ready for deploy github.io

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,4 +79,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  router: {
+    base: "/trpg-system/",
+  },
+
+  generate: {
+    dir: "dist",
+  },
 };

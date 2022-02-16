6e0ec141e1fa69ab1570b7eff0a1b0d4d970d31d
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2022-02-16 17:41:57 +0900

    pluguin化 & 薬師道具セットへの対応

--- pages/sw25simulate.vue
+++ pages/sw25simulate.vue
@@ -41,7 +41,7 @@ div
         )
         v-text-field(
           v-model="criticalRay",
-          label="【クリティカルレイ】などの出目が1回だけ+される効果",
+          label="【クリティカルレイ】などの1回だけ出目が+される効果",
           :rules="rules.numberRule",
           dense
         )
@@ -51,6 +51,13 @@ div
           :rules="rules.hissatsuNumberRule",
           dense
         )
+        v-text-field(
+          v-model="yakushi",
+          label="〈薬師道具セット〉などの1回だけ出目のうち1つを固定する効果",
+          :rules="rules.numberRule",
+          dense
+        )
+
       v-btn(block, @click="simulate") シミュレート
 
   SyachiCard(v-for="(result, i) in results", :key="i")
@@ -70,8 +77,9 @@ div
         | 威力 {{ result.keyNumber }}
         | &nbsp;&&nbsp;	追加ダメージ {{ result.additionalDamage }}
         | &nbsp;&&nbsp;	クリティカル値 {{ result.criticalNumber }}
-        span(v-if="result.criticalRay") &nbsp;&&nbsp;	クリティカルレイなど {{ result.criticalRay }}
-        span(v-if="result.hissatsu") &nbsp;&&nbsp;	必殺攻撃など {{ result.hissatsu }}
+        span(v-if="result.criticalRay && result.criticalRay != 0") &nbsp;&&nbsp;	クリティカルレイなど {{ result.criticalRay }}
+        span(v-if="result.hissatsu && result.hissatsu != 0") &nbsp;&&nbsp;	必殺攻撃など {{ result.hissatsu }}
+        span(v-if="result.yakushi && result.yakushi != 0") &nbsp;&&nbsp;	薬師道具セットなど {{ result.yakushi }}
         hr.my-2
         | &nbsp;=>&nbsp;ダメージ期待値 {{ result.damage }}
       //- br
@@ -102,14 +110,15 @@ export default Vue.extend({
       criticalNumber: 10,
       criticalRay: 0,
       hissatsu: 0,
+      yakushi: 0,
       results: [] as any[],
       rules: {
         numberRule: [
-          (v: any) => !!v || "入力必須です",
+          // (v: any) => !!v || "入力必須です",
           (v: any) => /\d+/.test(v) || "半角数字を入力してください",
         ],
         keyNumberRule: [
-          (v: any) => !!v || "入力必須です",
+          // (v: any) => !!v || "入力必須です",
           (v: any) => /\d+/.test(v) || "半角数字を入力してください",
           (v: any) => v >= 0 || "0以上の数字を入力してください",
           (v: any) => v <= 100 || "100以下の数字を入力してください",
@@ -120,190 +129,62 @@ export default Vue.extend({
           (v: any) => v >= 7 || "7以上の数字を入力してください",
         ],
         hissatsuNumberRule: [
-          (v: any) => !!v || "入力必須です",
+          // (v: any) => !!v || "入力必須です",
           (v: any) => /\d+/.test(v) || "半角数字を入力してください",
           (v: any) => v >= 0 || "0以上の数字を入力してください",
           (v: any) => v < 10 || "10未満の数字を入力してください",
         ],
       },
-      keyNumberTable: [
-        // 0
-        [0, 0, 0, 0, 1, 2, 2, 3, 3, 4, 4],
-        // 1
-        [0, 0, 0, 0, 1, 2, 3, 3, 3, 4, 4],
-        [0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4],
-        [0, 0, 0, 1, 1, 2, 3, 4, 4, 4, 5],
-        [0, 0, 0, 1, 2, 2, 3, 4, 4, 5, 5],
-        [0, 0, 1, 1, 2, 2, 3, 4, 5, 5, 5],
-        [0, 0, 1, 1, 2, 3, 3, 4, 5, 5, 5],
-        [0, 0, 1, 1, 2, 3, 4, 4, 5, 5, 6],
-        [0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6],
-        [0, 0, 1, 2, 3, 3, 4, 4, 5, 6, 7],
-        [0, 1, 1, 2, 3, 3, 4, 5, 5, 6, 7],
-        // 11
-        [0, 1, 2, 2, 3, 3, 4, 5, 6, 6, 7],
-        [0, 1, 2, 2, 3, 4, 4, 5, 6, 6, 7],
-        [0, 1, 2, 3, 3, 4, 4, 5, 6, 7, 7],
-        [0, 1, 2, 3, 4, 4, 4, 5, 6, 7, 8],
-        [0, 1, 2, 3, 4, 4, 5, 5, 6, 7, 8],
-        [0, 1, 2, 3, 4, 4, 5, 6, 7, 7, 8],
-        [0, 1, 2, 3, 4, 5, 5, 6, 7, 7, 8],
-        [0, 1, 2, 3, 4, 5, 6, 6, 7, 7, 8],
-        [0, 1, 2, 3, 4, 5, 6, 7, 7, 8, 9],
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        // 21
-        [0, 1, 2, 3, 4, 6, 6, 7, 8, 9, 10],
-        [0, 1, 2, 3, 5, 6, 6, 7, 8, 9, 10],
-        [0, 2, 2, 3, 5, 6, 7, 7, 8, 9, 10],
-        [0, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10],
-        [0, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10],
-        [0, 2, 3, 4, 5, 6, 8, 8, 9, 9, 10],
-        [0, 2, 3, 4, 6, 6, 8, 8, 9, 9, 10],
-        [0, 2, 3, 4, 6, 6, 8, 9, 9, 10, 10],
-        [0, 2, 3, 4, 6, 7, 8, 9, 9, 10, 10],
-        [0, 2, 4, 4, 6, 7, 8, 9, 10, 10, 10],
-        // 31
-        [0, 2, 4, 5, 6, 7, 8, 9, 10, 10, 11],
-        [0, 3, 4, 5, 6, 7, 8, 10, 10, 10, 11],
-        [0, 3, 4, 5, 6, 8, 8, 10, 10, 10, 11],
-        [0, 3, 4, 5, 6, 8, 9, 10, 10, 11, 11],
-        [0, 3, 4, 5, 7, 8, 9, 10, 10, 11, 12],
-        [0, 3, 5, 5, 7, 8, 9, 10, 11, 11, 12],
-        [0, 3, 5, 6, 7, 8, 9, 10, 11, 12, 12],
-        [0, 3, 5, 6, 7, 8, 10, 10, 11, 12, 13],
-        [0, 4, 5, 6, 7, 8, 10, 11, 11, 12, 13],
-        [0, 4, 5, 6, 7, 9, 10, 11, 11, 12, 13],
-        // 41
-        [0, 4, 6, 6, 7, 9, 10, 11, 12, 12, 13],
-        [0, 4, 6, 7, 7, 9, 10, 11, 12, 13, 13],
-        [0, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14],
-        [0, 4, 6, 7, 8, 10, 10, 11, 12, 13, 14],
-        [0, 4, 6, 7, 9, 10, 10, 11, 12, 13, 14],
-        [0, 4, 6, 7, 9, 10, 10, 12, 13, 13, 14],
-        [0, 4, 6, 7, 9, 10, 11, 12, 13, 13, 15],
-        [0, 4, 6, 7, 9, 10, 12, 12, 13, 13, 15],
-        [0, 4, 6, 7, 10, 10, 12, 12, 13, 14, 15],
-        [0, 4, 6, 8, 10, 10, 12, 12, 13, 15, 15],
-        // 51
-        [0, 5, 7, 8, 10, 10, 12, 12, 13, 15, 15],
-        [0, 5, 7, 8, 10, 11, 12, 12, 13, 15, 15],
-        [0, 5, 7, 9, 10, 11, 12, 12, 14, 15, 15],
-        [0, 5, 7, 9, 10, 11, 12, 13, 14, 15, 16],
-        [0, 5, 7, 10, 10, 11, 12, 13, 14, 16, 16],
-        [0, 5, 8, 10, 10, 11, 12, 13, 15, 16, 16],
-        [0, 5, 8, 10, 11, 11, 12, 13, 15, 16, 17],
-        [0, 5, 8, 10, 11, 12, 12, 13, 15, 16, 17],
-        [0, 5, 9, 10, 11, 12, 12, 14, 15, 16, 17],
-        [0, 5, 9, 10, 11, 12, 13, 14, 15, 16, 18],
-        // 61
-        [0, 5, 9, 10, 11, 12, 13, 14, 16, 17, 18],
-        [0, 5, 9, 10, 11, 13, 13, 14, 16, 17, 18],
-        [0, 5, 9, 10, 11, 13, 13, 15, 17, 17, 18],
-        [0, 5, 9, 10, 11, 13, 14, 15, 17, 17, 18],
-        [0, 5, 9, 10, 12, 13, 14, 15, 17, 18, 18],
-        [0, 5, 9, 10, 12, 13, 15, 15, 17, 18, 19],
-        [0, 5, 9, 10, 12, 13, 15, 16, 17, 19, 19],
-        [0, 5, 9, 10, 12, 14, 15, 16, 17, 19, 19],
-        [0, 5, 9, 10, 12, 14, 16, 16, 17, 19, 19],
-        [0, 5, 9, 10, 12, 14, 16, 17, 18, 19, 19],
-        // 71
-        [0, 5, 9, 10, 13, 14, 16, 17, 18, 19, 20],
-        [0, 5, 9, 10, 13, 15, 16, 17, 18, 19, 20],
-        [0, 5, 9, 10, 13, 15, 16, 17, 19, 20, 21],
-        [0, 6, 9, 10, 13, 15, 16, 18, 19, 20, 21],
-        [0, 6, 9, 10, 13, 16, 16, 18, 19, 20, 21],
-        [0, 6, 9, 10, 13, 16, 17, 18, 19, 20, 21],
-        [0, 6, 9, 10, 13, 16, 17, 18, 20, 21, 22],
-        [0, 6, 9, 10, 13, 16, 17, 19, 20, 22, 23],
-        [0, 6, 9, 10, 13, 16, 18, 19, 20, 22, 23],
-        [0, 6, 9, 10, 13, 16, 18, 20, 21, 22, 23],
-        // 81
-        [0, 6, 9, 10, 13, 17, 18, 20, 21, 22, 23],
-        [0, 6, 9, 10, 14, 17, 18, 20, 21, 22, 24],
-        [0, 6, 9, 11, 14, 17, 18, 20, 21, 23, 24],
-        [0, 6, 9, 11, 14, 17, 19, 20, 21, 23, 24],
-        [0, 6, 9, 11, 14, 17, 19, 21, 22, 23, 24],
-        [0, 7, 10, 11, 14, 17, 19, 21, 22, 23, 25],
-        [0, 7, 10, 12, 14, 17, 19, 21, 22, 24, 25],
-        [0, 7, 10, 12, 14, 18, 19, 21, 22, 24, 25],
-        [0, 7, 10, 12, 15, 18, 19, 21, 22, 24, 26],
-        [0, 7, 10, 12, 15, 18, 19, 21, 23, 25, 26],
-        // 91
-        [0, 7, 11, 13, 15, 18, 19, 21, 23, 25, 26],
-        [0, 7, 11, 13, 15, 18, 20, 21, 23, 25, 27],
-        [0, 8, 11, 13, 15, 18, 20, 22, 23, 25, 27],
-        [0, 8, 11, 13, 16, 18, 20, 22, 23, 25, 28],
-        [0, 8, 11, 14, 16, 18, 20, 22, 23, 26, 28],
-        [0, 8, 11, 14, 16, 19, 20, 22, 23, 26, 28],
-        [0, 8, 12, 14, 16, 19, 20, 22, 24, 26, 28],
-        [0, 8, 12, 15, 16, 19, 20, 22, 24, 27, 28],
-        [0, 8, 12, 15, 17, 19, 20, 22, 24, 27, 29],
-        [0, 8, 12, 15, 18, 19, 20, 22, 24, 27, 30],
-      ],
     };
   },
   methods: {
-    dice() {
-      return Math.ceil(Math.random() * 6);
-    },
-    twoDice() {
-      return this.dice() + this.dice();
-    },
     simulate() {
       const trial = 10 ** 5;
       // 出目のリセット
-      this.diceCount = 0;
-      this.diceSum = 0;
       let count = 0;
       let countHalf = 0;
       if (this.mode == "ダメージ期待値" || this.mode == "判定成否") {
-        for (let i = 0; i < trial; i++) {
-          const nodoDice = this.nodoKoteichi ? 0 : this.twoDice();
-          // 固定値ではない場合に処理を行う
-          if (!this.nodoKoteichi) {
-            this.diceCount++;
-            this.diceSum += nodoDice;
-            // 能動自動失敗
-            if (nodoDice == 2) continue;
+        [, countHalf, count] = this.$hanteiSimulate(
+          trial,
+          this.nodo - this.zyudo,
+          {
+            nodoKoteichi: this.nodoKoteichi,
+            zyudoKoteichi: this.zyudoKoteichi,
+            teiko: this.teiko,
           }
-          // 「抵抗：必中」の場合を処理する
-          if (this.teiko == "必中") {
-            count++;
-            continue;
-          }
-          const zyudoDice = this.zyudoKoteichi ? 0 : this.twoDice();
-          // 固定値ではない場合に処理を行う
-          if (!this.zyudoKoteichi) {
-            this.diceCount++;
-            this.diceSum += nodoDice;
-            // 受動自動成功
-            if (zyudoDice == 12) {
-              if (this.teiko == "半減") countHalf++;
-              continue;
-            }
-            // 能動自動成功 or 受動自動失敗
-            if (nodoDice == 12 || zyudoDice == 2) {
-              count++;
-              continue;
-            }
-          }
-          // 普通に上回る
-          if (nodoDice + Number(this.nodo) > zyudoDice + Number(this.zyudo)) {
-            count++;
-          } else {
-            if (this.teiko == "半減") countHalf++;
-          }
-        }
+        );
       } else if (this.mode == "ダメージ") {
         count = trial;
       }
       let damageCount = 0;
       if (this.mode == "ダメージ期待値" || this.mode == "ダメージ") {
         for (let j = 0; j < count; j++) {
-          damageCount += this.damage();
+          damageCount += this.$damage(
+            this.keyNumber,
+            this.additionalDamage,
+            this.criticalNumber,
+            {
+              half: false,
+              criticalRay: this.criticalRay,
+              hissatsu: this.hissatsu,
+              yakushi: this.yakushi,
+            }
+          );
         }
-        for (let h = 0; h < countHalf; h++) {
-          if (this.teiko == "半減") damageCount += this.damage("half");
+        if (this.teiko == "半減") {
+          for (let h = 0; h < countHalf; h++) {
+            damageCount += this.$damage(
+              this.keyNumber,
+              this.additionalDamage,
+              this.criticalNumber,
+              {
+                half: true,
+                criticalRay: this.criticalRay,
+                hissatsu: this.hissatsu,
+                yakushi: this.yakushi,
+              }
+            );
+          }
         }
       }
       if (this.mode == "ダメージ期待値") {
@@ -317,6 +198,7 @@ export default Vue.extend({
           criticalNumber: this.criticalNumber,
           criticalRay: this.criticalRay,
           hissatsu: this.hissatsu,
+          yakushi: this.yakushi,
           damage: damageCount / trial,
           kitaichi: this.diceSum / this.diceCount,
         });
@@ -333,48 +215,11 @@ export default Vue.extend({
           criticalNumber: this.criticalNumber,
           criticalRay: this.criticalRay,
           hissatsu: this.hissatsu,
+          yakushi: this.yakushi,
           damage: damageCount / trial,
           kitaichi: this.diceSum / this.diceCount,
         });
       }
-    },
-    damage(half?: string) {
-      const key = this.keyNumberTable[Number(this.keyNumber)];
-      let damageDice = this.twoDice();
-      this.diceCount++;
-      this.diceSum += damageDice;
-      // クリティカルレイ or 必殺攻撃
-      if (this.criticalRay || this.hissatsu) {
-        damageDice += Number(this.criticalRay) + Number(this.hissatsu);
-        if (damageDice > 12) damageDice = 12;
-      }
-      // 自動失敗
-      if (damageDice == 2) return 0;
-      // 成功した場合
-      let tempDamage = 0;
-      tempDamage += key[damageDice - 2] + Number(this.additionalDamage);
-      // 半減の場合はクリティカルしないのでここで返す
-      if (half) {
-        return Math.ceil(tempDamage / 2);
-      }
-      // クリティカル処理
-      criticalLoop: while (true) {
-        if (damageDice >= Number(this.criticalNumber)) {
-          tempDamage += key[damageDice - 2];
-        } else {
-          break criticalLoop;
-        }
-        damageDice = this.twoDice();
-        this.diceCount++;
-        this.diceSum += damageDice;
-
-        // 必殺攻撃
-        if (this.hissatsu) {
-          damageDice += Number(this.hissatsu);
-          if (damageDice > 12) damageDice = 12;
-        }
-      }
-      return tempDamage;
     },
   },
 });
---
+++ plugins/diceBot.ts
@@ -0,0 +1,253 @@
+import Vue from "vue";
+
+type Dice = 1 | 2 | 3 | 4 | 5 | 6;
+type TwoDice = 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+type HanteiOption = {
+  nodoKoteichi?: Boolean;
+  zyudoKoteichi?: Boolean;
+  teiko?: "必中" | "半減" | String;
+};
+
+type DamageOption = {
+  half?: Boolean;
+  criticalRay?: number;
+  hissatsu?: number;
+  yakushi?: number;
+};
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $dice: () => Dice;
+    $twoDice: (number?: number) => TwoDice;
+    $hantei: (difference: number, option: HanteiOption) => 0 | 1 | 2;
+    $hanteiSimulate: (
+      trial: number,
+      difference: number,
+      option: HanteiOption
+    ) => number[];
+    $damage: (
+      keyNumber: number,
+      additionalDamage: number,
+      criticalNumber: number,
+      option: DamageOption
+    ) => number;
+  }
+}
+
+const $dice = () => Math.ceil(Math.random() * 6) as Dice;
+const $twoDice = (number?: number) => {
+  if (number) return ($dice() + Number(number)) as TwoDice;
+  else return ($dice() + $dice()) as TwoDice;
+};
+
+const $hantei = (difference: number, option: HanteiOption) => {
+  const nodoDice = option.nodoKoteichi ? 0 : $twoDice();
+  // 固定値ではない場合に処理を行う
+  if (!option.nodoKoteichi) {
+    // 能動自動失敗
+    if (nodoDice == 2) return 0;
+  }
+  // 「抵抗：必中」の場合を処理する
+  if (option.teiko == "必中") {
+    return 2;
+  }
+  const zyudoDice = option.zyudoKoteichi ? 0 : $twoDice();
+  // 固定値ではない場合に処理を行う
+  if (!option.zyudoKoteichi) {
+    // 受動自動成功
+    if (zyudoDice == 12) {
+      if (option.teiko == "半減") return 1;
+      else return 0;
+    }
+    // 能動自動成功 or 受動自動失敗
+    if (nodoDice == 12 || zyudoDice == 2) {
+      return 2;
+    }
+  }
+  // 普通に上回る
+  if (nodoDice + difference > zyudoDice) {
+    return 2;
+  } else {
+    if (option.teiko == "半減") return 1;
+    return 0;
+  }
+};
+
+const $hanteiSimulate = (
+  trial: number,
+  difference: number,
+  option: HanteiOption
+) => {
+  const counts: number[] = [0, 0, 0];
+  for (let i = 0; i < trial; i++) {
+    counts[$hantei(difference, option)]++;
+  }
+  return counts;
+};
+
+const keyNumberTable = [
+  // 0
+  [0, 0, 0, 0, 1, 2, 2, 3, 3, 4, 4],
+  // 1
+  [0, 0, 0, 0, 1, 2, 3, 3, 3, 4, 4],
+  [0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4],
+  [0, 0, 0, 1, 1, 2, 3, 4, 4, 4, 5],
+  [0, 0, 0, 1, 2, 2, 3, 4, 4, 5, 5],
+  [0, 0, 1, 1, 2, 2, 3, 4, 5, 5, 5],
+  [0, 0, 1, 1, 2, 3, 3, 4, 5, 5, 5],
+  [0, 0, 1, 1, 2, 3, 4, 4, 5, 5, 6],
+  [0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6],
+  [0, 0, 1, 2, 3, 3, 4, 4, 5, 6, 7],
+  [0, 1, 1, 2, 3, 3, 4, 5, 5, 6, 7],
+  // 11
+  [0, 1, 2, 2, 3, 3, 4, 5, 6, 6, 7],
+  [0, 1, 2, 2, 3, 4, 4, 5, 6, 6, 7],
+  [0, 1, 2, 3, 3, 4, 4, 5, 6, 7, 7],
+  [0, 1, 2, 3, 4, 4, 4, 5, 6, 7, 8],
+  [0, 1, 2, 3, 4, 4, 5, 5, 6, 7, 8],
+  [0, 1, 2, 3, 4, 4, 5, 6, 7, 7, 8],
+  [0, 1, 2, 3, 4, 5, 5, 6, 7, 7, 8],
+  [0, 1, 2, 3, 4, 5, 6, 6, 7, 7, 8],
+  [0, 1, 2, 3, 4, 5, 6, 7, 7, 8, 9],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  // 21
+  [0, 1, 2, 3, 4, 6, 6, 7, 8, 9, 10],
+  [0, 1, 2, 3, 5, 6, 6, 7, 8, 9, 10],
+  [0, 2, 2, 3, 5, 6, 7, 7, 8, 9, 10],
+  [0, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10],
+  [0, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10],
+  [0, 2, 3, 4, 5, 6, 8, 8, 9, 9, 10],
+  [0, 2, 3, 4, 6, 6, 8, 8, 9, 9, 10],
+  [0, 2, 3, 4, 6, 6, 8, 9, 9, 10, 10],
+  [0, 2, 3, 4, 6, 7, 8, 9, 9, 10, 10],
+  [0, 2, 4, 4, 6, 7, 8, 9, 10, 10, 10],
+  // 31
+  [0, 2, 4, 5, 6, 7, 8, 9, 10, 10, 11],
+  [0, 3, 4, 5, 6, 7, 8, 10, 10, 10, 11],
+  [0, 3, 4, 5, 6, 8, 8, 10, 10, 10, 11],
+  [0, 3, 4, 5, 6, 8, 9, 10, 10, 11, 11],
+  [0, 3, 4, 5, 7, 8, 9, 10, 10, 11, 12],
+  [0, 3, 5, 5, 7, 8, 9, 10, 11, 11, 12],
+  [0, 3, 5, 6, 7, 8, 9, 10, 11, 12, 12],
+  [0, 3, 5, 6, 7, 8, 10, 10, 11, 12, 13],
+  [0, 4, 5, 6, 7, 8, 10, 11, 11, 12, 13],
+  [0, 4, 5, 6, 7, 9, 10, 11, 11, 12, 13],
+  // 41
+  [0, 4, 6, 6, 7, 9, 10, 11, 12, 12, 13],
+  [0, 4, 6, 7, 7, 9, 10, 11, 12, 13, 13],
+  [0, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+  [0, 4, 6, 7, 8, 10, 10, 11, 12, 13, 14],
+  [0, 4, 6, 7, 9, 10, 10, 11, 12, 13, 14],
+  [0, 4, 6, 7, 9, 10, 10, 12, 13, 13, 14],
+  [0, 4, 6, 7, 9, 10, 11, 12, 13, 13, 15],
+  [0, 4, 6, 7, 9, 10, 12, 12, 13, 13, 15],
+  [0, 4, 6, 7, 10, 10, 12, 12, 13, 14, 15],
+  [0, 4, 6, 8, 10, 10, 12, 12, 13, 15, 15],
+  // 51
+  [0, 5, 7, 8, 10, 10, 12, 12, 13, 15, 15],
+  [0, 5, 7, 8, 10, 11, 12, 12, 13, 15, 15],
+  [0, 5, 7, 9, 10, 11, 12, 12, 14, 15, 15],
+  [0, 5, 7, 9, 10, 11, 12, 13, 14, 15, 16],
+  [0, 5, 7, 10, 10, 11, 12, 13, 14, 16, 16],
+  [0, 5, 8, 10, 10, 11, 12, 13, 15, 16, 16],
+  [0, 5, 8, 10, 11, 11, 12, 13, 15, 16, 17],
+  [0, 5, 8, 10, 11, 12, 12, 13, 15, 16, 17],
+  [0, 5, 9, 10, 11, 12, 12, 14, 15, 16, 17],
+  [0, 5, 9, 10, 11, 12, 13, 14, 15, 16, 18],
+  // 61
+  [0, 5, 9, 10, 11, 12, 13, 14, 16, 17, 18],
+  [0, 5, 9, 10, 11, 13, 13, 14, 16, 17, 18],
+  [0, 5, 9, 10, 11, 13, 13, 15, 17, 17, 18],
+  [0, 5, 9, 10, 11, 13, 14, 15, 17, 17, 18],
+  [0, 5, 9, 10, 12, 13, 14, 15, 17, 18, 18],
+  [0, 5, 9, 10, 12, 13, 15, 15, 17, 18, 19],
+  [0, 5, 9, 10, 12, 13, 15, 16, 17, 19, 19],
+  [0, 5, 9, 10, 12, 14, 15, 16, 17, 19, 19],
+  [0, 5, 9, 10, 12, 14, 16, 16, 17, 19, 19],
+  [0, 5, 9, 10, 12, 14, 16, 17, 18, 19, 19],
+  // 71
+  [0, 5, 9, 10, 13, 14, 16, 17, 18, 19, 20],
+  [0, 5, 9, 10, 13, 15, 16, 17, 18, 19, 20],
+  [0, 5, 9, 10, 13, 15, 16, 17, 19, 20, 21],
+  [0, 6, 9, 10, 13, 15, 16, 18, 19, 20, 21],
+  [0, 6, 9, 10, 13, 16, 16, 18, 19, 20, 21],
+  [0, 6, 9, 10, 13, 16, 17, 18, 19, 20, 21],
+  [0, 6, 9, 10, 13, 16, 17, 18, 20, 21, 22],
+  [0, 6, 9, 10, 13, 16, 17, 19, 20, 22, 23],
+  [0, 6, 9, 10, 13, 16, 18, 19, 20, 22, 23],
+  [0, 6, 9, 10, 13, 16, 18, 20, 21, 22, 23],
+  // 81
+  [0, 6, 9, 10, 13, 17, 18, 20, 21, 22, 23],
+  [0, 6, 9, 10, 14, 17, 18, 20, 21, 22, 24],
+  [0, 6, 9, 11, 14, 17, 18, 20, 21, 23, 24],
+  [0, 6, 9, 11, 14, 17, 19, 20, 21, 23, 24],
+  [0, 6, 9, 11, 14, 17, 19, 21, 22, 23, 24],
+  [0, 7, 10, 11, 14, 17, 19, 21, 22, 23, 25],
+  [0, 7, 10, 12, 14, 17, 19, 21, 22, 24, 25],
+  [0, 7, 10, 12, 14, 18, 19, 21, 22, 24, 25],
+  [0, 7, 10, 12, 15, 18, 19, 21, 22, 24, 26],
+  [0, 7, 10, 12, 15, 18, 19, 21, 23, 25, 26],
+  // 91
+  [0, 7, 11, 13, 15, 18, 19, 21, 23, 25, 26],
+  [0, 7, 11, 13, 15, 18, 20, 21, 23, 25, 27],
+  [0, 8, 11, 13, 15, 18, 20, 22, 23, 25, 27],
+  [0, 8, 11, 13, 16, 18, 20, 22, 23, 25, 28],
+  [0, 8, 11, 14, 16, 18, 20, 22, 23, 26, 28],
+  [0, 8, 11, 14, 16, 19, 20, 22, 23, 26, 28],
+  [0, 8, 12, 14, 16, 19, 20, 22, 24, 26, 28],
+  [0, 8, 12, 15, 16, 19, 20, 22, 24, 27, 28],
+  [0, 8, 12, 15, 17, 19, 20, 22, 24, 27, 29],
+  [0, 8, 12, 15, 18, 19, 20, 22, 24, 27, 30],
+];
+
+const $damage = (
+  keyNumber: number,
+  additionalDamage: number,
+  criticalNumber: number,
+  option: DamageOption
+) => {
+  const key = keyNumberTable[keyNumber];
+  let damageDice: number;
+  if (option.yakushi) {
+    damageDice = $twoDice(option.yakushi);
+  } else {
+    damageDice = $twoDice();
+  }
+  // クリティカルレイ or 必殺攻撃
+  if (option.criticalRay || option.hissatsu) {
+    damageDice += Number(option.criticalRay) + Number(option.hissatsu);
+    if (damageDice > 12) damageDice = 12;
+  }
+  // 自動失敗
+  if (damageDice == 2) return 0;
+  // 成功した場合
+  let damage = 0;
+  damage += key[damageDice - 2] + additionalDamage;
+  // 半減の場合とC値が13以上の場合はクリティカルしないのでここで返す
+  if (option.half || criticalNumber >= 13) {
+    return Math.ceil(damage / 2);
+  }
+  // クリティカル処理
+  criticalLoop: while (true) {
+    if (damageDice >= criticalNumber) {
+      damage += key[damageDice - 2];
+    } else {
+      break criticalLoop;
+    }
+    damageDice = $twoDice();
+
+    // 必殺攻撃
+    if (option.hissatsu) {
+      damageDice += Number(option.hissatsu);
+      if (damageDice > 12) damageDice = 12;
+    }
+  }
+  return damage;
+};
+
+Vue.prototype.$dice = $dice;
+Vue.prototype.$twoDice = $twoDice;
+Vue.prototype.$hantei = $hantei;
+Vue.prototype.$hanteiSimulate = $hanteiSimulate;
+Vue.prototype.$damage = $damage;

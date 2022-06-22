cdd8dce4ab93260d6b915aa4c7922669c9941439
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2022-06-22 09:43:26 +0900

    add meta

--- pages/myScenario/pudding.vue
+++ pages/myScenario/pudding.vue
@@ -14,7 +14,7 @@ div
       template(v-for="data in scenario.data")
         span.data {{ data }}
         br
-  Scenario(scenarioId="pudding")
+  Scenario(:scenarioId="scenarioId")
 </template>
 
 <script lang="ts">
@@ -25,15 +25,58 @@ import Scenario from "@/components/scenario/index.vue";
 
 import { scenarioById } from "@/assets/scenarios";
 
+const scenarioId = "pudding";
+
 export default Vue.extend({
   name: "PuddingScenarioPage",
+  meta: [
+    {
+      hid: "description",
+      name: "description",
+      content: scenarioById(scenarioId).messages.join("/n"),
+    },
+    {
+      hid: "description",
+      name: "description",
+      content: scenarioById(scenarioId).messages.join("/n"),
+    },
+    {
+      hid: "og:site_name",
+      property: "og:site_name",
+      content: scenarioById(scenarioId).title,
+    },
+    { hid: "og:type", property: "og:type", content: "website" },
+    {
+      hid: "og:url",
+      property: "og:url",
+      content: `https://syachi-hoko-maru.github.io/trpg-system/myScenario/${scenarioId}`,
+    },
+    {
+      hid: "og:title",
+      property: "og:title",
+      content: scenarioById(scenarioId).title,
+    },
+    {
+      hid: "og:description",
+      property: "og:description",
+      content: scenarioById(scenarioId).messages.join("/n"),
+    },
+    {
+      hid: "og:image",
+      property: "og:image",
+      content: scenarioById(scenarioId).src
+        ? require(`~/assets/projects/${scenarioById(scenarioId).src}`)
+        : `https://syachi-hoko-maru.github.io/trpg-system/syachi.png`,
+    },
+  ],
   components: {
     SyachiCard,
     Scenario,
   },
   data: function () {
     return {
-      scenario: scenarioById("pudding"),
+      scenario: scenarioById(scenarioId),
+      scenarioId: scenarioId,
     };
   },
 });

d35f8f08a15c482d132ad1609c4a57a83bc4be7e
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-03-14 13:16:46 +0900

    lastmodの表記方法を変更

--- src/pages/pageSettingList.ts
+++ src/pages/pageSettingList.ts
@@ -5,7 +5,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "トップページ",
     to: "/",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     img: "ogp.png",
     specialPage: true,
     tags: [],
@@ -13,13 +13,13 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "ソード・ワールド2.5",
     to: "/sw25",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     tags: ["sw25"],
   },
   {
     title: "ソード・ワールド2.5 ツール一覧",
     to: "/sw25/tool",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     img: "page-image/tool.png",
     osusume: true,
     explain:
@@ -29,7 +29,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "SW2.5 判定シミュレータ",
     to: "/sw25/tool/simulate",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     img: "page-image/simulate.png",
     osusume: true,
     explain:
@@ -39,7 +39,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "オンセ募集用画像作成ツール",
     to: "/sw25/tool/bosyu",
-    lastmod: "2023-3-9",
+    lastmod: "2023/3/9",
     img: "page-image/bosyu.png",
     osusume: true,
     explain:
@@ -49,7 +49,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "SW2.5 Twitterコミュニティ紹介",
     to: "/sw25/tool/community",
-    lastmod: "2023-3-8",
+    lastmod: "2023/3/8",
     img: "page-image/community.png",
     osusume: true,
     explain: "SW2.5に関するTwitterコミュニティの紹介ページです。",
@@ -58,7 +58,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "オンラインセッション用素材",
     to: "/sw25/tool/material",
-    lastmod: "2023-3-8",
+    lastmod: "2023/3/8",
     img: "page-image/material.png",
     osusume: true,
     explain:
@@ -68,7 +68,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "SW2.5 オリジナルデータ",
     to: "/sw25/data",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     // osusume: true,
     hidden: true,
     explain:
@@ -78,7 +78,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "SW2.5 初心者向け情報",
     to: "/sw25/forbeginner",
-    lastmod: "2023-3-13",
+    lastmod: "2023/3/13",
     // osusume: true,
     hidden: true,
     explain: "ソドワ初心者向けの、ソード・ワールド2.5情報をまとめています。",
@@ -87,7 +87,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "SW2.5 おすすめサプリメント紹介",
     to: "/sw25/forbeginner/suppliment",
-    lastmod: "2023-3-13",
+    lastmod: "2023/3/13",
     img: "page-image/suppliment.png",
     osusume: true,
     explain:
@@ -97,7 +97,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "公開シナリオ一覧",
     to: "/scenario",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     img: "page-image/scenario.png",
     explain:
       "管理人しゃちほこ丸が公開しているシナリオ一覧です。ソード・ワールド2.5やエモクロアTRPGのシナリオを公開しています。",
@@ -106,7 +106,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "おすすめシナリオ",
     to: "/scenario/osusume",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     osusume: true,
     explain: "管理人しゃちほこ丸がおすすめのSW2.5シナリオ一覧です。",
     tags: ["sw25", "scenario"],
@@ -115,7 +115,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "管理人の自己紹介",
     to: "/me",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     img: "page-image/me.png",
     explain: "管理人しゃちほこ丸の自己紹介ページです。",
     osusume: true,
@@ -124,7 +124,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "所持ルールブック一覧",
     to: "/me/rulebook",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     img: "page-image/rulebook.png",
     explain:
       "管理人しゃちほこ丸が所持しているルールブック、サプリメントの一覧ページです。",
@@ -133,7 +133,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "ハウスルール",
     to: "/me/houserule",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     img: "page-image/house.png",
     explain:
       "管理人しゃちほこ丸が主催するセッションでのハウスルールを掲示しているページです。",
@@ -142,7 +142,7 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "SW2.5 ハウスルール",
     to: "/me/houserule/sw25",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     img: "page-image/house.png",
     explain:
       "管理人しゃちほこ丸が主催するSW2.5のセッションでのハウスルールを掲示しているページです。",
@@ -151,14 +151,14 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "ブログ",
     to: "/blog",
-    lastmod: "2023-3-10",
+    lastmod: "2023/3/10",
     explain: "管理人しゃちほこ丸が思ったことを綴っています。",
     tags: ["me"],
   },
   {
     title: "検索",
     to: "/search",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     specialPage: true,
     explain: "当ウェブサイトの内容を検索できます。",
     tags: [],
@@ -166,21 +166,21 @@ const normalPageSettingList: PageSetting[] = [
   {
     title: "設定",
     to: "/setting",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     specialPage: true,
     tags: [],
   },
   {
     title: "サイトポリシー",
     to: "/policy",
-    lastmod: "2023-3-7",
+    lastmod: "2023/3/7",
     specialPage: true,
     tags: [],
   },
   {
     title: "エラーページ",
     to: "error",
-    lastmod: "2023-3-1",
+    lastmod: "2023/3/1",
     tags: [],
   },
 ];

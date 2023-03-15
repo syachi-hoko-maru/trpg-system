ccf4c478a41eab8c0e5fb87fe303a54daacf094d
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-03-15 18:22:59 +0900

    最近更新されたページ

--- components/layouts/tailCard/index.vue
+++ components/layouts/tailCard/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <card-array-by-andml v-if="pageSetting.tags.indexOf('sw25_new') >= 0" :andml="andml" />
+  <card-array-by-andml v-if="pageSetting.tags.indexOf('sw25_new') >= 0" :andml="sourceAndml" />
   <layouts-tail-card-osusume v-if="pageSetting.to !== '/policy'" :page-setting="pageSetting" type="kanren" />
   <item-amazon v-if="pageSetting.tags.findIndex(tag => tag.indexOf('sw25') >= 0) >= 0" />
   <card>
@@ -11,6 +11,17 @@
     </template>
     <item-share :text="shareSetting.text" :url="shareSetting.url">このページをツイート</item-share>
     ぜひTwitterでシェアして広めてください
+  </card>
+  <card>
+    <template #title>
+      最近更新されたページ
+    </template>
+    <div v-for="[date, pages] of Object.entries(recent)" :key="date" class="mt-2">
+      {{ date }}
+      <div v-for="page of pages" :key="page.to" class="pl-3">
+        <atom-link :to="page.to" deco>{{ page.title }}</atom-link>
+      </div>
+    </div>
   </card>
   <layouts-tail-card-osusume v-if="pageSetting.to !== '/policy'" :page-setting="pageSetting" />
 </template>
@@ -50,7 +61,18 @@ const shareSetting = computed(() => {
   }
 })
 
-const andml = `
+const recent: { [date: string]: PageSetting[] } = {}
+let count = 0
+$pageSettingList.filter(p => !p.hidden).sort((a, b) => new Date(b.lastmod).getTime() - new Date(a.lastmod).getTime()).slice(0, 15).forEach(p => {
+  if (!recent[p.lastmod] || !recent[p.lastmod].length) {
+    if (count < 10) recent[p.lastmod] = []
+    else return
+  }
+  recent[p.lastmod].push(p)
+  count++
+})
+
+const sourceAndml = `
 &1 ソース
 SW2.5の新刊情報は主に以下より入手しています。
 - &link_http://www.groupsne.co.jp/news/book.html,グループSNEオフィシャルサイト「新刊情報」

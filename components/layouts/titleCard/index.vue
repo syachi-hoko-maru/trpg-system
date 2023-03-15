d9893556e018b9946a7399745df0e45e7aeffda0
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-03-15 19:01:25 +0900

    ブログの時だけdateを表示

--- components/layouts/titleCard/index.vue
+++ components/layouts/titleCard/index.vue
@@ -1,6 +1,7 @@
 <template>
   <card v-show="!pageSetting.specialPage">
     <template #title>{{ pageSetting.title }}</template>
+    <template #subtitle v-if="pageSetting.to.indexOf('/blog/') === 0">{{ pageSetting.lastmod }}</template>
     <template #after v-if="pageSetting.tags?.length">
       <item-tags :tags="pageSetting.tags" class="py-0" />
     </template>

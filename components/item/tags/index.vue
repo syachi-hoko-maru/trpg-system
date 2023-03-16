3c3130a911d03126a5024f081a01d83c559caa30
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-03-16 12:38:17 +0900

    tagを修正

--- components/item/tags/index.vue
+++ components/item/tags/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="tags.length" class="my-3">
+  <div v-if="tags.length" class="mt-0 mb-3 mx-2">
     <item-tags-tag v-for="tag, i of tags" :pageTag="tag" :pageTagSetting="$pageTagSettings[tag]" :key="`${tag}_${i}`" />
   </div>
 </template>

d257d588f7f99f39c8cda896e06cd3e1cc8ebc30
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-03-16 13:58:13 +0900

    altを少し追加

--- components/layouts/titleCard/index.vue
+++ components/layouts/titleCard/index.vue
@@ -8,7 +8,7 @@
   </card>
   <card v-if="!pageSetting.specialPage && pageSetting.img">
     <template #tbefore>
-      <v-img :src="`${$templateText.basePath}/webp/${pageSetting.img}`" alt="サムネイル画像" />
+      <v-img :src="`${$templateText.basePath}/webp/${pageSetting.img}`" :alt="`${pageSetting.title}サムネイル画像`" />
     </template>
   </card>
 </template>

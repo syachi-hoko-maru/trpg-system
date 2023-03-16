e7836748798cbcb4b924cd3666f8a43f9d43a7e5
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-03-16 12:25:55 +0900

    複数ページ用機能を追加

--- composables/pages.ts
+++ composables/pages.ts
@@ -2,12 +2,17 @@ export const usePages = () => {
   const route = useRoute();
 
   const pageArray = useState("pageArray", () => [] as string[]);
-  // const changePageFuncArray = useState(
-  //   "changePageFuncArray",
-  //   () => [] as { key: string; func: () => void }[]
-  // );
 
   const getNowPage = () => pageArray.value[0];
+  const getNowPagePage = (): number => {
+    const qp = Number(
+      Array.isArray(route.params.page)
+        ? route.params.page[0]
+        : route.params.page
+    );
+    if (Number.isNaN(qp)) return 1;
+    else return qp;
+  };
   const getNowPath = () =>
     getNowPage()?.replace(/\?.*$/, "").replace(/#.*$/, "");
   const getQuerys = () => {
@@ -33,27 +38,15 @@ export const usePages = () => {
     const pageUrl = p.replace(/(.+)\/+$/, "$1");
     if (pageUrl !== pageArray.value[0]) pageArray.value.unshift(pageUrl);
     console.log("save page", pageArray.value);
-    // changePageFuncArray.value.forEach(({ key, func }) => {
-    //   console.log(key);
-    //   func();
-    // });
   };
-  // const saveChangePageFunc = (key: string, func: () => void) => {
-  //   const i = changePageFuncArray.value.findIndex((obj) => key === obj.key);
-  //   if (i >= 0) {
-  //     changePageFuncArray.value[i].func = func;
-  //   } else {
-  //     changePageFuncArray.value.push({ key, func });
-  //   }
-  // };
 
   return {
     pageArray,
     getNowPage,
+    getNowPagePage,
     getNowPath,
     getQuerys,
     getQueryValue,
     savePage,
-    // saveChangePageFunc,
   };
 };

f0af245436f8dfc4533c4e37d64955ac89ed7147
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-03-20 16:55:26 +0900

    FAQに追加

--- pages/sw25/forbeginner/index.vue
+++ pages/sw25/forbeginner/index.vue
@@ -18,7 +18,9 @@
 
   <card>
     <template #title>
-      便利なリンク
+      <div id="links">
+        便利なリンク
+      </div>
     </template>
     <andml :andmls="andmlll" />
     <card v-for="link of links" :key="link.title">
@@ -43,11 +45,18 @@ const showFAQ = ref(true)
 
 const faqs = [
   {
+    title: "SW2.5って何の略ですか？　他にソード・ワールド2.5の略称はありますか？",
+    explain: [
+      "SW2.5はTRPG「ソード・ワールド2.5」の略称の1つです。",
+      "他にも「sw25」や「ソドワ」「そどわ」などが使われます。",
+    ],
+  },
+  {
     title: "SW2.5ってどんなTRPGですか？",
     explain: [
       "このウェブサイトでは以下で紹介しています。",
       "&button_/sw25 ",
-      "また、以下の「便利なリンク」の上の方にもソード・ワールド2.5について紹介しているスライドやページを紹介しています。参考にしてください。"
+      "また、以下の &link_#links,「便利なリンク」 の上の方にもソード・ワールド2.5について紹介しているスライドやページを紹介しています。参考にしてください。"
     ],
   },
   {
@@ -55,6 +64,12 @@ const faqs = [
     explain: [
       "「ソード・ワールド2.5」（SW2.5）も「クトゥルフ神話TRPG」（CoC）も &em_TRPGのシステムの1つ です。",
       "トークによって物語を遊ぶというTRPGの本質は同じですが、世界観やルールに違いがあります。"
+    ],
+  },
+  {
+    title: "SW2.5を始めるにはどうすればいいですか？",
+    explain: [
+      "このページの &link_#links,「便利なリンク」 を順に読んでくだされば始められます！",
     ],
   },
   {
@@ -82,7 +97,6 @@ const faqs = [
       "ただ、ルールブックを「ワールド」だけでも読んで臨んだほうが &em_よりソード・ワールド2.5を楽しめると思う ので、買っておくことをお勧めです。"
     ],
   },
-
   {
     title: "SW2.5のサプリメントはどれを買えばいいですか？",
     explain: [
@@ -90,6 +104,15 @@ const faqs = [
       "&br",
       "その上で、「サプリメントを買うぜ！」という場合には以下を参照ください。",
       "&button_/sw25/forbeginner/suppliment/1"
+    ],
+  },
+  {
+    title: "SW2.5と他のファンタジーTRPGとの違いはなんですか？",
+    explain: [
+      "日本のファンタジー系TRPGの中では、まずわかりやすく &em_「手軽さ」と毎年複数冊のサプリメントが出る「サポートの手厚さ」 によって差別化されます。",
+      "&br",
+      "さまざまなルールの違いに関しては &link_https://www.youtube.com/@Baketsusan,バケツさん（YouTube） のシリーズ「ファンタジーTRPGを比べてみよう！」がおすすめです。",
+      "&button_https://www.youtube.com/watch?v=2ibJQwxapyE&list=PLKNsDN1V6QW66JwABhpATxu9uySNjOP-1&index=1 YouTube"
     ],
   },
 

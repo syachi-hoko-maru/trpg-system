10dab8670821ae4d66def3760ef85289741495f5
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-03-26 14:42:03 +0900

    cmsの修正とアニポケブログの公開

--- src/gene/setBlogData/cms.ts
+++ src/gene/setBlogData/cms.ts
@@ -5,7 +5,7 @@ import { blogTextDir } from "../index";
 export const getCmsData = async () => {
   const data = (
     await axios.get(
-      "https://script.googleusercontent.com/macros/echo?user_content_key=Rt5ATiduHaaLpU7TSG4BTGrQaAlRMixa9zds5nDmoCKDu9b8r-0GeogDALTKsycMTo_Hq_qyXWOarsM41o0zPXlt4Iso6liom5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnN_QV3DEyrgkQ-AfOU2C0x9t5GoLDuqO3wdnRTX4QRBasnrioFWZnkGFe8a_2iO5y_g80m9Jf0kc5ikwVsIAbAoitqJ_28Cxuw&lib=McmnOCEslCnBuDjO1eYZTjYp_ku7yiUEV"
+      "https://script.googleusercontent.com/macros/echo?user_content_key=hHiF2dG8NAXWy50xMTmQAewtpO9H36PJNnn-oaTQczA8QUe27Zs5IVRbsj0i5rUcolOG1G4h-kf28JJN6FILHL_IvVUVezzBm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnN_QV3DEyrgkQ-AfOU2C0x9t5GoLDuqO3wdnRTX4QRBasnrioFWZnkGFe8a_2iO5y_g80m9Jf0kc5ikwVsIAbAoitqJ_28Cxuw&lib=McmnOCEslCnBuDjO1eYZTjYp_ku7yiUEV"
     )
   ).data as unknown;
   if (
@@ -13,8 +13,8 @@ export const getCmsData = async () => {
     Array.isArray(data[0]) &&
     typeof data[0][0] === "string"
   ) {
-    (data as string[][]).forEach((line, i) => {
-      if (i === 0 || !line[1] || !line.slice(2).join("\n")) return;
+    (data as string[][]).forEach((line) => {
+      if (!line[1] || !line.slice(2).join("\n")) return;
       console.log(`save ${line[1]}.txt`);
       writeFileSync(
         `${blogTextDir}/cms/${line[1]}.txt`,

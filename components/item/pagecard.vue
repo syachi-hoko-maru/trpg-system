5efee1984fbb02b7cba6f4a8eee47e485f8a19a0
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-03-27 15:32:22 +0900

    ページカードのタグが押せない問題を修正

--- components/item/pagecard.vue
+++ components/item/pagecard.vue
@@ -1,33 +1,37 @@
 <template>
-  <atom-link :to="pageSettingData.to" v-if="pageSettingData">
-    <card class="my-5">
-      <template #tbefore>
-        <v-container class="pa-0" v-if="pageSettingData.img">
-          <v-row :height="120">
-            <v-col cols="3">
-              <v-img :src="`${$templateText.basePath}/webp/${pageSettingData.img}`" :height="120" cover class="mb-2" />
-            </v-col>
-            <v-col cols="8">
-              <v-row class="pt-5">
+  <card class="my-5" v-if="pageSettingData">
+    <template #tbefore>
+      <v-container class="pa-0" v-if="pageSettingData.img">
+        <v-row :height="120">
+          <v-col cols="3">
+            <v-img :src="`${$templateText.basePath}/webp/${pageSettingData.img}`" :height="120" cover class="mb-2" />
+          </v-col>
+          <v-col cols="8">
+            <v-row class="pt-5">
+              <atom-link :to="pageSettingData.to">
                 <v-card-title class="search-card-title">{{ pageSettingData.title }}</v-card-title>
                 <v-card-subtitle class="search-card-title" v-if="pageSettingData.to.indexOf('/blog/') === 0">
                   {{ pageSettingData.lastmod }}
                 </v-card-subtitle>
-                <item-tags :tags="pageSettingData.tags" class="py-0" v-if="mounted" />
-              </v-row>
-            </v-col>
-          </v-row>
-        </v-container>
-        <div v-else>
+              </atom-link>
+              <item-tags :tags="pageSettingData.tags" class="py-0" v-if="mounted" />
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+      <div v-else>
+        <atom-link :to="pageSettingData.to">
           <v-card-title class="search-card-title">{{ pageSettingData.title }}</v-card-title>
-          <item-tags :tags="pageSettingData.tags" class="py-0" v-if="mounted" />
-        </div>
+        </atom-link>
+        <item-tags :tags="pageSettingData.tags" class="py-0" v-if="mounted" />
+      </div>
+      <atom-link :to="pageSettingData.to">
         <v-card-text class="pt-0" v-if="pageSettingData.explain && pageSettingData.explain.length">
           <andml :andmls="pageSettingData.explain" />
         </v-card-text>
-      </template>
-    </card>
-  </atom-link>
+      </atom-link>
+    </template>
+  </card>
 </template>
 
 <script setup lang="ts">

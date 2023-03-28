545b02893bc3464a160f9b338ba165aa6815dcc4
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-03-28 16:39:36 +0900

    cardへのリンクでヘッダーとの重なりを意識

--- components/card/index.vue
+++ components/card/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :id="id">
+  <div class="card-div" :id="id">
     <v-card class="my-5" :class="Props.class ? Props.class : 'bg-background text-text'" elevation="3">
       <v-row v-if="Props.loading" class="cardLoading align-center justify-center">
         <v-progress-circular :size="70" :width="7" indeterminate color="primary" />
@@ -47,5 +47,12 @@ const Props = defineProps<Props>()
   right: 0;
   bottom: 0;
   background-color: rgba(128, 128, 128, 0.5);
+}
+
+div.card-div[id]::before {
+  display: block;
+  height: 55px;
+  margin-top: -75px;
+  content: "";
 }
 </style>

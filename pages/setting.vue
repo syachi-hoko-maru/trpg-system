9c1aecf5178fa520aec6259d8ea5b29c59262982
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-03-31 10:00:28 +0900

    設定画面に設定の保存を追加

--- pages/setting.vue
+++ pages/setting.vue
@@ -17,10 +17,16 @@
 import { useTheme } from 'vuetify'
 
 const theme = useTheme()
+const { setSnack } = useSnack()
 
 const toggleTheme = () => {
   theme.global.name.value = theme.global.current.value.dark ? "myCustomLightTheme" : "myCustomDarkTheme"
-  localStorage.setItem('theme', theme.global.current.value.dark ? "dark" : "light");
+  try {
+    localStorage.setItem('theme', theme.global.current.value.dark ? "dark" : "light");
+    setSnack("設定を保存しました")
+  } catch {
+    setSnack("設定の保存に失敗しました")
+  }
 }
 
 const { pageArray } = usePages()

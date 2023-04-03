788927735adc5cb95f78692a69c3e896c52f5125
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-04-03 18:14:28 +0900

    強調しないワードの自動削除

--- components/andml/inline/emc.vue
+++ components/andml/inline/emc.vue
@@ -1,7 +1,8 @@
 <template>
-  <em class="em" :style="`background: linear-gradient(transparent 50%, ${$vuetify.theme.current.colors.accent} 50%);`">
-    <andml-inline-spanc :props="props" />
-  </em>
+  {{ prelinktext }}<em class="em"
+    :style="`background: linear-gradient(transparent 50%, ${$vuetify.theme.current.colors.accent} 50%);`">
+    <andml-inline-spanc :props="linktext" />
+  </em>{{ postlinktext }}
 </template>
 
 <script setup lang="ts">
@@ -9,6 +10,27 @@ interface Props {
   props: string
 }
 const Props = defineProps<Props>();
+
+const linktext = ref(Props.props)
+const prelinktext = ref("")
+const postlinktext = ref("")
+
+const notEmWords = ["。", "、"]
+for (let w of notEmWords) {
+  if (Props.props.endsWith(w)) {
+    linktext.value = Props.props.replace(new RegExp(`${w}$`), "")
+    postlinktext.value = w
+  }
+}
+
+const kakomiWords = [["「", "」"]]
+for (let w of kakomiWords) {
+  if (Props.props.startsWith(w[0]) && Props.props.endsWith(w[1])) {
+    linktext.value = Props.props.replace(new RegExp(`^${w[0]}(.*)${w[1]}$`), "$1")
+    prelinktext.value = w[0]
+    postlinktext.value = w[1]
+  }
+}
 
 </script>
 

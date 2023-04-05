d69586b7fe95e521088d0da969601a6f0670e6bc
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-04-05 19:28:15 +0900

    辞書の更新

--- src/dict/index.ts
+++ src/dict/index.ts
@@ -2,4 +2,10 @@ import { ginouWordList } from "./ginou";
 import { syuzokuWordList } from "./syuzoku";
 
 export const wordList: () => WordList = () =>
-  Object.assign(syuzokuWordList(), ginouWordList());
+  Object.assign(syuzokuWordList(), ginouWordList(), [
+    {
+      流派: "ソード・ワールド2.5における流派は、より多彩な戦闘スタイルを実現するための戦闘特技や魔法のようなもの。サプリメントによって追加された。",
+      キャンペーン:
+        "TRPGでは同じメンバー、同じキャラクターで続けて遊ぶことをキャンペーンという。",
+    },
+  ]);

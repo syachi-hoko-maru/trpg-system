35d030f5b4d48566a457c17a04cf401f4baf3a14
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-04-06 09:29:50 +0900

    dictを修正

--- src/dict/index.ts
+++ src/dict/index.ts
@@ -2,10 +2,8 @@ import { ginouWordList } from "./ginou";
 import { syuzokuWordList } from "./syuzoku";
 
 export const wordList: () => WordList = () =>
-  Object.assign(syuzokuWordList(), ginouWordList(), [
-    {
-      流派: "ソード・ワールド2.5における流派は、より多彩な戦闘スタイルを実現するための戦闘特技や魔法のようなもの。サプリメントによって追加された。",
-      キャンペーン:
-        "TRPGでは同じメンバー、同じキャラクターで続けて遊ぶことをキャンペーンという。",
-    },
-  ]);
+  Object.assign(syuzokuWordList(), ginouWordList(), {
+    流派: "ソード・ワールド2.5における流派は、より多彩な戦闘スタイルを実現するための戦闘特技や魔法のようなもの。サプリメントによって追加された。",
+    キャンペーン:
+      "TRPGでは同じメンバー、同じキャラクターで続けて遊ぶことをキャンペーンという。",
+  });

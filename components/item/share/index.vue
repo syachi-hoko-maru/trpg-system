30f92d777bfcd2f642aa91337379883667ed9999
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-04-16 11:52:23 +0900

    ハッシュタグのカスタマイズ機能

--- components/item/share/index.vue
+++ components/item/share/index.vue
@@ -13,9 +13,11 @@ const { $templateText } = useNuxtApp()
 interface Props {
   url?: string,
   text: string,
-  icon?: boolean
+  icon?: boolean,
+  hashTag?: string,
 }
 const Props = defineProps<Props>();
 
-const shareUrl = computed(() => `https://twitter.com/share?url=${Props.url ? Props.url : $templateText.baseUrl}&text=${encodeURIComponent(Props.text)}&related=@syachi_hoko_trp&hashtags=${encodeURIComponent("TRPGギルド支部しゃちほこの尾びれ亭")}`)
+const hashtags = `&hashtags=${encodeURIComponent(Props.hashTag ? Props.hashTag : "TRPGギルド支部しゃちほこの尾びれ亭")}`
+const shareUrl = computed(() => `https://twitter.com/share?url=${Props.url ? Props.url : $templateText.baseUrl}&text=${encodeURIComponent(Props.text)}&related=@syachi_hoko_trp${hashtags}`)
 </script>

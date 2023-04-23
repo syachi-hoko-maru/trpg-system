b8e9fa5de4d95db99d3f8ddaeb649c9004a280d4
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-04-23 14:04:42 +0900

    amazonを少し改造

--- components/item/amazon.vue
+++ components/item/amazon.vue
@@ -17,21 +17,21 @@
       Amazon商品リンク
     </template>
     <div>
-      <p class="mb-3">
-        こちらから購入いただくとAmazonから紹介料をいただけます。<br>
-        活動支援としてポチっていただけたら幸いです。
-      </p>
-      <item-scrollx>
-        <div v-for="item of items" :key="item.title" class="mx-2">
-          <iframe :title="`${item.title}のAmazonリンク`" loading="lazy"
-            sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
-            style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0"
-            :src="item.src" />
-        </div>
-      </item-scrollx>
-      <item-button url="https://amzn.to/3T30UvM">
-        「ソード・ワールド2.5」を検索
-      </item-button>
+      <andml :andmls="andml1" />
+      <div class="my-5">
+        <item-scrollx>
+          <div v-for="item of items" :key="item.title" class="mx-2">
+            <iframe :title="`${item.title}のAmazonリンク`" loading="lazy"
+              sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+              style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0"
+              :src="item.src" />
+          </div>
+        </item-scrollx>
+        <item-button url="https://amzn.to/3T30UvM">
+          「ソード・ワールド2.5」を検索
+        </item-button>
+      </div>
+      <andml :andmls="andml2" />
       <item-caption>
         ※ Amazonアソシエイト・プログラムに参加しています
       </item-caption>
@@ -45,6 +45,17 @@ interface Props {
   item?: string[]
 }
 const Props = defineProps<Props>();
+
+const andml1 = `
+こちらから購入いただくとAmazonから紹介料をいただけます。
+&em_活動支援としてポチっていただけたら 幸いです。
+`
+
+const andml2 = `
+※ おすすめのサプリメントについては以下をご覧ください。
+&button_/sw25/forbeginner/suppliment
+
+`
 
 const items = [
   {

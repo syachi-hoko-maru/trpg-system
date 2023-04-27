e1f8b22cf2773b71dff03c2117cb238b17bd26ca
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-04-27 11:34:35 +0900

    ダークモードへの切り替えボタンをコンポーネント化

---
+++ components/pages/setting/darkmode.vue
@@ -0,0 +1,22 @@
+<template>
+  <item-button @click="toggleTheme">
+    {{ theme.global.current.value.dark ? "ライトモード" : "ダークモード" }}に切り替える
+  </item-button>
+</template>
+
+<script setup lang="ts">
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+const { setSnack } = useSnack()
+
+const toggleTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? "myCustomLightTheme" : "myCustomDarkTheme"
+  try {
+    localStorage.setItem('theme', theme.global.current.value.dark ? "dark" : "light");
+    setSnack("設定を保存しました")
+  } catch {
+    setSnack("設定の保存に失敗しました")
+  }
+}
+</script>
--- pages/setting.vue
+++ pages/setting.vue
@@ -3,9 +3,7 @@
     <template #title>
       設定
     </template>
-    <item-button @click="toggleTheme">
-      {{ theme.global.current.value.dark ? "ライトモード" : "ダークモード" }}に切り替える
-    </item-button>
+    <pages-setting-darkmode />
     <item-button v-if="pageArray && pageArray.length >= 2" :to="pageArray[1]">
       1つ前のページへ戻る
     </item-button>
@@ -14,22 +12,5 @@
 </template>
 
 <script setup lang="ts">
-import { useTheme } from 'vuetify'
-
-const theme = useTheme()
-const { setSnack } = useSnack()
-
-const toggleTheme = () => {
-  theme.global.name.value = theme.global.current.value.dark ? "myCustomLightTheme" : "myCustomDarkTheme"
-  try {
-    localStorage.setItem('theme', theme.global.current.value.dark ? "dark" : "light");
-    setSnack("設定を保存しました")
-  } catch {
-    setSnack("設定の保存に失敗しました")
-  }
-}
-
 const { pageArray } = usePages()
-
-const { $templateText } = useNuxtApp()
 </script>

f14c7cf3e4c41cae8e3ae40f8dc1daaf76616b7f
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-04-27 12:05:11 +0900

    amazonバナーを更新

--- components/item/amazon/box.vue
+++ components/item/amazon/box.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="div-amazon" class="align-center">
-    <iframe
+    <!-- <iframe
       src="https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=12&l=ur1&category=monthlydealevent&banner=140Z9CGTRPHE20SMKXR2&f=ifr&linkID=ee2c29e44c1bafe3501c352230bdb1aa&t=syachihokotrp-22&tracking_id=syachihokotrp-22"
       width="300" height="250" scrolling="no" border="0" marginwidth="0" id="amazon" style="border:none;" frameborder="0"
+      sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"></iframe> -->
+    <iframe
+      src="https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=12&l=ur1&category=mothersday23&banner=0EBJTC10YSKTZX6VN6R2&f=ifr&linkID=94052bee623c691e558744d3276339e6&t=syachihokotrp-22&tracking_id=syachihokotrp-22"
+      width="300" height="250" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"
       sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"></iframe>
   </div>
 </template>
 
 <script setup lang="ts">
 const setFrame = () => {
-  const frame = document.getElementById("amazon")
   const div = document.getElementById("div-amazon")
-  if (!frame || !div) return
+  if (!div) return
+  const frame = div.querySelector("iframe")
+  if (!frame) return
   if (div.clientWidth < 300) {
     console.log(div.clientWidth, frame.clientWidth)
     frame.style.transform = `scale(${div.clientWidth / frame.clientWidth})`

14b465da582e0b1e9a1fa4db1be08ba8c71fa6bb
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-04-27 19:19:14 +0900

    head3の更新

--- components/item/head2.vue
+++ components/item/head2.vue
@@ -1,5 +1,12 @@
 <template>
-  <h3 class="text-h6 mt-3 mb-1">
+  <h3 class="head2 mt-3 mb-1">
     <slot />
   </h3>
 </template>
+
+<style lang="scss" scoped>
+h3.head2 {
+  font-size: 1.3em;
+  font-weight: bold;
+}
+</style>

a2b9d1c8a67aeaf0f37bf6b08b3982094e44817a
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-05-04 11:16:59 +0900

    画像コンポーネントにaspectRatioを追加

--- components/item/img.vue
+++ components/item/img.vue
@@ -1,20 +1,17 @@
 <template>
-  <v-img :src="src" :lazy-src="src" :alt="alt" :width="width" :height="height" :cover="cover" />
+  <v-img :src="src" :lazy-src="src" :alt="alt" :class="class" :width="width" :height="height" :aspect-ratio="aspectRatio"
+    :cover="cover" />
 </template>
 
 <script setup lang="ts">
 interface Props {
   src: string,
   alt: string,
+  class?: string,
   width?: string,
   height?: string,
   cover?: boolean
+  aspectRatio?: string
 }
 const Props = defineProps<Props>();
 </script>
-
-<style scoped lang="scss">
-img.image {
-  width: 100%;
-}
-</style>

0e59cc02cb41dbc239fe796db93420b51e507aac
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-05-08 20:11:07 +0900

    footerに gifteeを追加し、ちょっと調整

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -1,14 +1,14 @@
 <template>
   <v-footer absolute app class="bg-secondary d-block" id="footer" :class="fixed ? 'fixed' : ''">
     <div class="my-5" v-if="!error">
-      <div class="my-2">
-        <atom-link color="text-textbp" to="/setting" title="設定ページへのリンク">
+      <div class="mt-2 mb-4">
+        <atom-link color="text-textbp" to="/setting" title="設定ページへのリンク（フッター）">
           <v-icon icon="mdi-wrench" size="small" class="px-3" />
           設定
         </atom-link>
       </div>
-      <div class="my-1" v-for="item of footerItemList" :key="item.to">
-        <atom-link color="text-textbp" :to="item.to" :title="`ページ「${item.title}」へのリンク`">{{ item.title }}</atom-link>
+      <div class="py-2" v-for="item of footerItemList" :key="item.to">
+        <atom-link color="text-textbp" :to="item.to" :title="`ページ「${item.title}」へのリンク（フッター）`">{{ item.title }}</atom-link>
       </div>
     </div>
     <div class="mt-5 mb-3">
@@ -32,6 +32,7 @@ const footerItemList = [
   { to: "/search", title: "ページ一覧・検索" },
   { to: "/policy", title: "サイトポリシー" },
   { to: "/contact", title: "コンタクト" },
+  { to: "https://giftee.com/u/syachi_hoko_trp", title: "欲しいものリスト（giftee）" },
 ]
 </script>
 

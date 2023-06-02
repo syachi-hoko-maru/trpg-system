66be430cb8d3a21445331785414ab3c358655c56
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-06-02 15:56:23 +0900

    andml blockにnoandmlを追加

---
+++ components/andml/block/noandml.vue
@@ -0,0 +1,12 @@
+<template>
+    <div v-for="line of lines">
+        {{ line }}
+    </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    lines?: string[]
+}
+const Props = defineProps<Props>();
+</script>
--- components/andml/index.vue
+++ components/andml/index.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <component :is="d.component" v-for="d of dataArray" :props="d.props">
+  <component :is="d.component" v-for="d of dataArray" :props="d.props" :lines="d.andmls">
     <andml-line :andmls="d.andmls" />
   </component>
 </template>
@@ -18,6 +18,7 @@ const andmlArray = computed(() => Array.isArray(Props.andmls) ? Props.andmls : P
 
 // lineの処理
 const andmlBlockScriptArray: AndmlScript[] = [
+  { script: "noandml", component: resolveComponent("AndmlBlockNoandml") },
   { script: "byosya", component: resolveComponent("AndmlBlockByosya") },
   { script: "forgm", component: resolveComponent("AndmlBlockForgm") },
   { script: "info", component: resolveComponent("AndmlBlockInfo") },

1744a72f3e8da313702543cb3c28bdcc83c25513
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-06-10 17:45:28 +0900

    head3のデザインを調整

--- components/item/head3.vue
+++ components/item/head3.vue
@@ -1,6 +1,5 @@
 <template>
   <h4 class="head3 mt-4 mb-2">
-    ◆
     <slot />
   </h4>
 </template>
@@ -8,6 +7,15 @@
 <style lang="scss" scoped>
 h4.head3 {
   font-size: 1.25em;
+  line-height: 1.5rem;
   font-weight: bold;
+  margin-left: 1.5rem;
+
+  &::before {
+    content: '◆';
+    display: block;
+    position: absolute;
+    transform: translate(-1.5rem, 0)
+  }
 }
 </style>

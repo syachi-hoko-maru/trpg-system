e9eaa9b6622e40a95217c0968b4228a0cde83e31
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-06-20 12:52:17 +0900

    シナノシマ関連の整理

---
+++ components/pages/shinano/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <CardArrayByAndml :andml="andml1" />
+    <slot />
+    <CardArrayByAndml :andml="andml2" />
+</template>
+
+<script setup lang="ts">
+const andml1 = `
+&1 このページについて
+このシナリオはしゃちほこ丸によるオリジナルサプリメント『シナノシマ¥s妖との戦い』の要素紹介ページです。
+詳しくはこちらもご覧ください。
+&button_/sw25/data/shinano
+`
+
+const andml2 = `
+&1 『シナノシマ 妖との戦い』へ戻る
+&button_/sw25/data/shinano
+`
+
+</script>

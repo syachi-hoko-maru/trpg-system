cfb578b56981793f8df25a7489955334f62dda31
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-09-14 14:34:56 +0900

    load.tsにコメントを追加

--- composables/load.ts
+++ composables/load.ts
@@ -1,23 +1,36 @@
 export const useLoad = () => {
+  // load中のものの配列（load中ならfalse）
   const load = useState("load", () => [] as boolean[]);
+  // 全てのloadが終わっているかどうかの値
   const ok = useState("ok", () => false);
-  const setLoad = () => {
+
+  // loadとして設定する
+  const setLoad = (): (() => void) => {
+    // falseとして設定する
     const i = load.value.push(false) - 1;
-    // console.log("set load", i);
+    // 返り値として、load終了したらtrueにするための関数
     return () => {
+      // すでにtrueなら終了
       if (load.value[i]) return;
-      // console.log("finish load", i);
+      // trueでないならtrueにする
       load.value[i] = true;
     };
   };
 
   const route = useRoute();
+  // ページが変わるたびに行う処理
   watch(route, () => {
+    // load中の配列を空にする
     load.value = [];
+    // 全てのloadが終わっているかどうかをfalseにする
     ok.value = false;
   });
+
+  // loadが進むたびに行う処理
   watch(load.value, () => {
+    // 全部がtrueなら（falseのものがないなら）
     if (load.value.filter((l) => !l).length === 0) {
+      // okの値を変える
       ok.value = true;
     } else {
       ok.value = false;

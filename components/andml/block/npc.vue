9958816925f6f1e2d8bf44635859dfb9898e41d2
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-10-01 15:22:47 +0900

    ウォフと更新

--- components/andml/block/npc.vue
+++ components/andml/block/npc.vue
@@ -1,7 +1,7 @@
 <template>
-  <item-block color="#345c58">
+  <item-block-copy color="#345c58" title="NPC情報">
     <template #title>NPC情報</template>
     <slot />
-  </item-block>
+  </item-block-copy>
 </template>
 

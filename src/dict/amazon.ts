63e02d57e13c02d35245509be98b04bb731a29f9
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2023-10-28 19:02:27 +0900

    amazonリンクの外部化

---
+++ src/dict/amazon.ts
@@ -0,0 +1,129 @@
+export const amazonItems: {
+  title: string;
+  src: string;
+  genre: "sw25" | "other";
+}[] = [
+  {
+    title: "ラクシアライフ",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040751868&linkId=267fd8a2abcbf8e95f134363fbf2dc72",
+    genre: "sw25",
+  },
+  {
+    title: "ギルドマスター・ウォーロックの依頼録",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040752325&linkId=17370b28e22e2f9795a586c466619def",
+    genre: "sw25",
+  },
+  {
+    title: "アーケインレリック",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040750640&linkId=a2dbb02933e972a78fbe9abd89190817",
+    genre: "sw25",
+  },
+  {
+    title: "ノベル+シナリオ 猫と星と秘宝",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040751167&linkId=946eccfc502b3e03bee511b5111b2656",
+    genre: "sw25",
+  },
+
+  {
+    title: "リプレイ 時の魔域と秘宝の守り人",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040750241&linkId=6d2bf0e7dbf9aaf6f8fa577e14236b7e",
+    genre: "sw25",
+  },
+  {
+    title: "異世界冒険ガイド2",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B0C3ZHJ68W&linkId=30059ced40c6b20087ec994b0ed8a53b",
+    genre: "sw25",
+  },
+  {
+    title: "エンシェントブルー",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040749146&linkId=531153ff4d2e8cfb3ae449814ab8dd12",
+    genre: "sw25",
+  },
+  {
+    title: "ブルライト博物誌",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=404074876X&linkId=5dd76d077ddec9f2e9c0737ca601d36d",
+    genre: "sw25",
+  },
+  // {
+  //   title: "ルールブックI",
+  //   src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040728076&linkId=caab8103d8951f3873671769f5b34c52"
+  // },
+  // {
+  //   title: "ルールブックII",
+  //   src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040728114&linkId=7edd08670f55d98302891d10389f15bd"
+  // },
+  // {
+  //   title: "ルールブックIII",
+  //   src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040728122&linkId=b5838eb57d409fdd1b3dc14fe79c681b"
+  // },
+  {
+    title: "エピックトレジャリー",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040733711&linkId=a7e11b51f30b22ba9a73c8d8a37d3677",
+    genre: "sw25",
+  },
+  {
+    title: "モンストラスロア",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040738209&linkId=f3fee570c86df68a12cc5abc5d4c9796",
+    genre: "sw25",
+  },
+  {
+    title: "メイガスアーツ",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040742834&linkId=a5825d6b386671cb1760f7f39e8883aa",
+    genre: "sw25",
+  },
+  {
+    title: "バトルマスタリー",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040747003&linkId=37cb411fa79d8c5271089bb8d071801a",
+    genre: "sw25",
+  },
+  {
+    title: "アウトロープロファイルブック",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040741617&linkId=c186cd01eae19a61bd222de58eb8917f",
+    genre: "sw25",
+  },
+  {
+    title: "魔導の学府ユーシズ",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040743571&linkId=700ccbc814466b9d63db95badcf788c1",
+    genre: "sw25",
+  },
+  {
+    title: "ヴァイスシティ",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040731581&linkId=d1f7922005cbb59b2b72244745db728a",
+    genre: "sw25",
+  },
+  {
+    title: "デモンズライン",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040737261&linkId=2b12e3f9a5cf86ce9cd0f8cb417e5d75",
+    genre: "sw25",
+  },
+  {
+    title: "グリフォンロード",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040744721&linkId=1df8349424f1b1ddb8ef4d06c84ef72b",
+    genre: "sw25",
+  },
+  {
+    title: "異世界冒険ガイド",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B0B3432N2N&linkId=2ae2556161d75a1d0352544df9aa8a9f",
+    genre: "sw25",
+  },
+  {
+    title: "異世界冒険ガイド 異世界武器ぺディア",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=404074764X&linkId=9b14ba0bb637abb2157bee3e39c9cdf5",
+    genre: "sw25",
+  },
+  {
+    title: "リプレイ ALLグララン総進撃!",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4040748549&linkId=13191204b52e8df9727f5079c4d977fe",
+    genre: "sw25",
+  },
+  {
+    title: "GMウォーロック",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4775320904&linkId=5d472c4b9250a8e37f3a06425f193214",
+    genre: "sw25",
+  },
+  {
+    title: "TRPGのデザイン",
+    src: "//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=syachihokotrp-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B0C4TFYGF6&linkId=7b6e37a646ee45449ad904f81b603455",
+    genre: "other",
+  },
+];

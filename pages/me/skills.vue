98ec29b4014334725b0765cc295c6a2c19da65f1
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2024-01-16 11:03:28 +0900

    スキルページを少しだけ更新

--- pages/me/skills.vue
+++ pages/me/skills.vue
@@ -11,10 +11,12 @@ const andml1 = `
 「やりたいこと」を「仕様」や「実装方針」にする工程が特に好きで、得意であると自負しています。
 
 &3 使用言語・フレームワーク
-- HTML5・CSS3
+- HTML5
+- CSS3
 - JavaScript（Node.js/TypeScript）
 -- React（PoC・個人開発のみ）
 -- Vue/nuxt
+--- Vue3/nuxt3
 -- Angular（PoCのみ）
 -- Express（個人開発のみ）
 - PHP
@@ -44,10 +46,30 @@ const andml1 = `
 - GitHub¥sActions
 - Google¥sApps¥sScript（個人開発のみ）
 
+&2 作品
+&3 当サイト
+- 使用技術
+-- フロントエンド：TypeScript¥s+¥sNuxt3¥s+¥sGitHub¥sPages
+-- デプロイ：GitHub¥sActions
+-- バックエンド（CMS）：Google¥sApps¥sScript（現在停止中）
+- 工夫点
+-- 完全無料デプロイ
+-- Google¥sApps¥sScriptとGitHub¥sActionsを使用した独自CMS
+--- Google¥sFormの送信時にGoogle¥sApps¥sScriptからGitHub¥sActionsを呼び、自動デプロイを行う
+-- 通勤時間にスマホで書きやすいように作成した独自マークアップ言語
+&button_https://github.com/syachi-hoko-maru/trpg-system/ GitHub
+
 &1 その他
 - Java（個人開発のみ）
 -- MinecraftのMOD開発など
-- Adobe¥s illustrator
+- Adobe¥sIllustrator
+-- 同人誌の表紙の編集経験あり
+- Adobe¥sInDesign（初学者）
 - Microsoft¥sOffice
+
+&2 作品
+&3 SW2.5同人誌『冒険者の旅路』表紙・裏表紙編集
+表紙イラストはたつみ先生
+&button_/sw25/feature/c102
 `
 </script>

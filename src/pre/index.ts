94f1f147b965b949c2f9786f29377684b2bbf225
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2024-01-21 23:12:41 +0900

    setSecretをコメントアウト

--- src/pre/index.ts
+++ src/pre/index.ts
@@ -2,4 +2,5 @@ import { setBlogData } from "./gene/setBlogData";
 import { setSecret } from "./setSecret";
 import { setwebps } from "./setwebps";
 
-setBlogData().then(setwebps).then(setSecret);
+setBlogData().then(setwebps);
+// .then(setSecret);

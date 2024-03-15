4d5396413ab82aa43685974c0dd041b8ca120b5d
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2024-03-15 18:50:31 +0900

    未使用ファイルをコメントアウト

--- src/dict/suppliment.ts
+++ src/dict/suppliment.ts
@@ -1,25 +1,25 @@
-import type { DateLike } from "../util/date";
-import { supplimentYesNo } from "./yesno/supplimentYesNo";
+// import type { DateLike } from "../util/date";
+// import { supplimentYesNo } from "./yesno/supplimentYesNo";
 
-export const supplimentList = [
-  { name: "エピックトレジャリー", type: "大型サプリ", tags: [], date: "" },
-  { name: "モンストラスロア", type: "大型サプリ", tags: [], date: "" },
-  { name: "メイガスアーツ", type: "大型サプリ", tags: [], date: "" },
-  { name: "バトルマスタリー", type: "大型サプリ", tags: [], date: "" },
-  {
-    name: "アーケインレリック",
-    type: "大型サプリ",
-    tags: [],
-    date: "2023/7/20",
-  },
-  //   { name: "バトルマスタリー", type: "大型サプリ", tags: [], date: "" },
-] as const satisfies Readonly<
-  {
-    name: string;
-    type: "大型サプリ";
-    tags: readonly (keyof typeof supplimentYesNo)[];
-    date: DateLike;
-  }[]
->;
+// export const supplimentList = [
+//   { name: "エピックトレジャリー", type: "大型サプリ", tags: [], date: "" },
+//   { name: "モンストラスロア", type: "大型サプリ", tags: [], date: "" },
+//   { name: "メイガスアーツ", type: "大型サプリ", tags: [], date: "" },
+//   { name: "バトルマスタリー", type: "大型サプリ", tags: [], date: "" },
+//   {
+//     name: "アーケインレリック",
+//     type: "大型サプリ",
+//     tags: [],
+//     date: "2023/7/20",
+//   },
+//   //   { name: "バトルマスタリー", type: "大型サプリ", tags: [], date: "" },
+// ] as const satisfies Readonly<
+//   {
+//     name: string;
+//     type: "大型サプリ";
+//     tags: readonly (keyof typeof supplimentYesNo)[];
+//     date: DateLike;
+//   }[]
+// >;
 
-export type SupplimentList = (typeof supplimentList)[number]["name"];
+// export type SupplimentList = (typeof supplimentList)[number]["name"];

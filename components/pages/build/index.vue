303233e2fa9517231acc7fb1529be3916730f246
syachi-hoko-maru <realmadrid.dmplay.s@gmail.com>
2024-03-18 18:26:45 +0900

    SW2.5・ソドワをSW2.5/ソドワに統一

--- components/pages/build/index.vue
+++ components/pages/build/index.vue
@@ -1,20 +1,26 @@
 <template>
   <card-array-by-andml :andml="andml1" />
-  <item-pagesindex title="他のキャラクタービルド"
-    :page-setting-list="$pageSettingList.filter(pa => pa.to.indexOf('/sw25/forbeginner/build/') === 0)" />
+  <item-pagesindex
+    title="他のキャラクタービルド"
+    :page-setting-list="
+      $pageSettingList.filter(
+        (pa) => pa.to.indexOf('/sw25/forbeginner/build/') === 0
+      )
+    "
+  />
   <card-array-by-andml :andml="andml2" />
 </template>
 
 <script setup lang="ts">
 interface Props {
-  aboutThisPage: string,
-  main: string
+  aboutThisPage: string;
+  main: string;
 }
 const Props = defineProps<Props>();
 
 const andml1 = `
 &1 このページについて
-このページはソード・ワールド2.5（SW2.5・ソドワ）におけるキャラクターのビルド（データ構成）の作り方を初心者向けにわかりやすく解説するシリーズの1ページです。
+このページはソード・ワールド2.5（SW2.5/ソドワ）におけるキャラクターのビルド（データ構成）の作り方を初心者向けにわかりやすく解説するシリーズの1ページです。
 &br
 ${Props.aboutThisPage}
 &br
@@ -29,7 +35,7 @@ ${Props.main}
 &1 キャラクタービルドをいろいろ解説しています
 &em_パーティーのバランスなど基本 が気になる方や、 &em_他のキャラクタービルド が気になる方は以下のページをご覧ください。
 &button_/sw25/forbeginner/build
-`
+`;
 
 const andml2 = `
 &1 併せて読んで欲しいページ
@@ -39,6 +45,5 @@ const andml2 = `
 また、ソード・ワールド2.5などのTRPGのキャラクターシートを保管・共有できるWebサービス「ゆとシート」の使い方を以下で公開しています。
 &button_/sw25/forbeginner/ytsheet
 ぜひどれもご覧ください。
-`
-
+`;
 </script>
